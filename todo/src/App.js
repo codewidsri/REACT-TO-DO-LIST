27f9bb6158b8da6e0634d7fc82dea2422d55da31
codewidsri <sriharishkumar17@gmail.com>
2024-07-24 23:12:27 +0530

    todo app initialised

---
+++ todo/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      todo app
+    </div>
+  );
+}
+
+export default App;
